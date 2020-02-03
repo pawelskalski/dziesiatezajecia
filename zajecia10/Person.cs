7683a0b59293d198921631b536541ceb20923938
Paweł Skalski <40362366+pawelskalski@users.noreply.github.com>
2020-02-03 13:34:21 +0100

    Koniec pracy

---
+++ zajecia10/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace zajecia10
+{
+    public class Person
+    {
+        public Person(int id, string name, string lastName, string phone)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            Phone = phone;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ zajecia10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace zajecia10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("data.txt");
+
+            List<Person> people = lines.Select(x =>
+            {
+                string[] data = x.Split(",");
+                return new Person(Convert.ToInt32(data[0]), data[1], data[2], data[3]);
+            }).ToList();
+
+            var sortedPeople = people.OrderBy(x => x.LastName)
+                .ThenBy(x => x.Name);
+
+            using (StreamWriter writer = new StreamWriter("result.txt"))
+            {
+                foreach (var item in sortedPeople)
+                {
+                    writer.WriteLine($"[{item.Id}] {item.Name} {item.LastName}: {item.Phone}");
+                }
+            }
+        }
+    }
+}
